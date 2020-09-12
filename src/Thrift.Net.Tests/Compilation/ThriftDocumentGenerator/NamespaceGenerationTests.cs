302d494d4929b540ff69ab80a81ce117a9db1dca
Adam Connelly <adam.rpconnelly@gmail.com>
2020-09-12 17:49:11 +0100

    fix(compiler): only generate namespace if supplied
    - Updated the code generation so that it only generates a namespace declaration if one is specified,
      and otherwise generates the document members in the root namespace.

--- src/Thrift.Net.Tests/Compilation/ThriftDocumentGenerator/NamespaceGenerationTests.cs
+++ src/Thrift.Net.Tests/Compilation/ThriftDocumentGenerator/NamespaceGenerationTests.cs
@@ -3,6 +3,7 @@ namespace Thrift.Net.Tests.Compilation.ThriftDocumentGenerator
     using System.Collections.Generic;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
     using Thrift.Net.Compilation.Symbols;
+    using Thrift.Net.Compilation.Symbols.Builders;
     using Xunit;
     using ThriftDocumentGenerator = Thrift.Net.Compilation.ThriftDocumentGenerator;
 
@@ -23,6 +24,29 @@ namespace Thrift.Net.Tests.Compilation.ThriftDocumentGenerator
             var root = ParseCSharp(output);
             var namespaceDeclaration = root.Members.First() as NamespaceDeclarationSyntax;
             Assert.Equal("Thrift.Net.Tests", namespaceDeclaration.Name.ToString());
+        }
+
+        [Fact]
+        public void Generate_NamespaceNotSupplied_DoesNotGenerateNamespace()
+        {
+            // Arrange
+            var generator = new ThriftDocumentGenerator();
+            var document = new ThriftDocument(
+                null,
+                new List<Enum>
+                {
+                    new EnumBuilder().SetName("UserType").Build(),
+                },
+                new List<Struct>());
+
+            // Act
+            var output = generator.Generate(document);
+
+            // Assert
+            var root = ParseCSharp(output);
+            var enumDeclaration = Assert.IsAssignableFrom<EnumDeclarationSyntax>(
+                root.Members.First());
+            Assert.Equal("UserType", enumDeclaration.Identifier.Text);
         }
     }
 }
