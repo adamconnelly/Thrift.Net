0111ecdf75151771da176cf7c446bdcc1f457603
Adam Connelly <adam.rpconnelly@gmail.com>
2020-11-22 13:46:08 +0000

    ci: don't lint Dependabot commit messages
    Our commit message linting fails for Dependabot commits because it doesn't reference an issue.
    For now I'm just excluding commit messages starting with `chore(deps` from linting to work around
    this.
    
    Part of #87

--- commitlint-ci.config.js
+++ commitlint-ci.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   extends: ["./commitlint.config.js"],
   defaultIgnores: false, // Don't allow things like `fixup!` when running against PRs
+  ignores: [
+    // Don't lint Dependabot commit messages because it doesn't reference an issue
+    // and will fail linting
+    (commit) => commit.startsWith("chore(deps"),
+  ],
 };
